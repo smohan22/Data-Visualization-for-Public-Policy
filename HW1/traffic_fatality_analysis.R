cfcb9308af18768d69bc3655aad4bd8f33e77ceb
smohan22 <shambhavimohan@gmail.com>
2017-10-04 13:52:02 -0500

    part2 done

--- HW1/traffic_fatality_analysis.R
+++ HW1/traffic_fatality_analysis.R
@@ -1,6 +1,8 @@
 #Shambhavi Mohan
 # This is an exploratory analysis of the DoT FARS data.
 
+setwd("~/Documents/GitHub/Data-Visualization-for-Public-Policy/HW1/")
+
 #import libraries
 library(readr)
 library(haven)
@@ -9,8 +11,26 @@ library(ggplot2)
 library(stringr)
 library(tidyr)
 
-# read in files
+#read in files
 acc2014 <- read_sas('accident.sas7bdat')
 acc2015 <- read_csv('accident.csv')
-
+#check for loaded
 ls()
+
+
+#Combining the two years of FARS data
+
+#change blank to null
+acc2014 <- mutate(acc2014, TWAY_ID2 = na_if(TWAY_ID2,""))
+table(is.na(acc2014$TWAY_ID2))
+#check for dimensions
+dim(acc2014)
+dim(acc2015)
+#Identifying missing data
+colnames(acc2014)
+colnames(acc2014) %in% colnames(acc2015)
+#missing data from acc2014: "ROAD_FNC"
+colnames(acc2015)
+colnames(acc2015) %in% colnames(acc2014)
+#missing data from acc2014: "RUR_URB",FUNC_SYS","RD_OWNER"
+
