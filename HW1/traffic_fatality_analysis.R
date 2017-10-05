42607440f03b8cac358e8067b2412aa10ca86240
smohan22 <shambhavimohan@gmail.com>
2017-10-04 21:02:42 -0500

    part4 done, Final HW ready for submission

--- HW1/traffic_fatality_analysis.R
+++ HW1/traffic_fatality_analysis.R
@@ -40,7 +40,7 @@ count(acc, RUR_URB)
 #Since RUR_URB was missing from acc2014, when we merged the two datasets, 
 #they were blank for acc2014. Hence NA in the merged set
 
-#Merging on another data source
+#PART3: Merging on another data source
 fips <- read_csv('fips.csv')
 glimpse(fips)
 
@@ -53,3 +53,23 @@ acc <- rename(acc, StateFIPSCode = STATE)
 acc <- rename(acc, CountyFIPSCode = COUNTY)
 # merge using join to not lose data
 acc_full <- acc %>% left_join(fips, by = c("StateFIPSCode","CountyFIPSCode"))
+
+#PART4: Exploratory Data Analysis in R’s dplyr and tidyr package
+agg <- acc_full %>%
+  group_by(YEAR, StateName) %>%
+  summarise(TOTAL = sum(FATALS))
+agg_wide <- spread(agg, YEAR, TOTAL)
+agg_wide <- mutate(agg_wide, Percent_Diff = (`2015`-`2014`) /`2014`)
+agg_wide <- arrange(agg_wide, Percent_Diff )
+agg_wide <- filter(agg_wide, Percent_Diff > 0.15)
+agg_wide <- filter(agg_wide, !is.na(StateName))
+
+#aggregation using chain
+agg <- acc_full %>%
+  group_by(YEAR, StateName) %>%
+  summarise(TOTAL = sum(FATALS)) %>%
+  spread(YEAR, TOTAL) %>%
+  mutate(Percent_Diff = (`2015`-`2014`) /`2014`) %>%
+  arrange(Percent_Diff ) %>%
+  filter(Percent_Diff > 0.15) %>%
+  filter(!is.na(StateName))
